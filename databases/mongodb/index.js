b7b6bb955eb2cc6d7b3c32c615f97ced14994dda
Oussama Seffai <Oussama.seffa@legal-doctrine.com>
2022-12-15 16:17:08 +0100

    (fix/mongodb) : fix connection issues

--- databases/mongodb/index.js
+++ databases/mongodb/index.js
@@ -3,16 +3,16 @@ const logger = require("../../utils/logger");
 require("dotenv").config();
 let { MONGO_DB_PORT } = process.env;
 
-exports.connectDB = async () => {
-  try {
-    logger.info(`connection to mongodb://localhost:${MONGO_DB_PORT}/enginedb`);
-    await mongoose.connect(`mongodb://localhost:${MONGO_DB_PORT}/enginedb`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    logger.info("MongoDB connected");
-  } catch (error) {
-    logger.error(error.message);
-    process.exit(1);
-  }
-};
+mongoose.set("strictQuery", false);
+
+mongoose.connect(`mongodb://localhost:${MONGO_DB_PORT}/enginedb`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  logger.info("connected to db");
+});
+mongoose.connection.on("disconnected", (err) => {
+  logger.error("disconected", err);
+});
